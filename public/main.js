2c9921ba92db456df59c8b177e6830d02b393d34
rochimensi <rochibass@gmail.com>
2017-10-27 14:48:58 -0300

    [Feature] 2 players room with Socket Messages

--- public/main.js
+++ public/main.js
@@ -1,7 +1,85 @@
 let socket = io.connect('http://localhost:8080', { 'forceNew': true });
+let questions = [];
+let questionNum = 0;
+let score = 0;
 
 function sendName(e) {
   const username = document.getElementById('username').value;
-  socket.emit('hello', username);
+  socket.emit('HELLO', username);
   return false;
 }
+
+function next() {
+  $('#result').hide();
+  $('#third').show();
+  var html = getQuestionHTML(questions[questionNum]);
+  $('#third').html(html);
+  $('#fourth').hide();
+}
+
+function answerQuestion(answer) {
+  socket.emit('ANSWER', {answer: answer.value, id: answer.id});
+  questionNum++;
+}
+
+function getQuestionHTML(q) {
+  let options = '';
+  q.options.forEach((o) => {
+    options += `<div class="option"><a onclick="answerQuestion({value: '${o}', id: ${q.id}})">${o}</a></div>`;
+  });
+
+  var html = `<div>
+        <h3>${q.question}</h3>
+        <p><strong>Categoría:</strong> ${q.subject}</p>
+        ${options}
+    `;
+
+  return html;
+}
+
+socket.on('ANSWER_ACK', (data) => {
+  $('#result').show();
+  $('#fourth').show();
+  if(data.correct) {
+    $('#result').html('<div class="result result--correct">Correcto!!</div>');
+    score++;
+    $('#score').html('<p><strong>Score: ' + score + ' / ' + questions.length + '</strong></p>');
+  } else {
+    $('#result').html('<div class="result result--incorrect">Incorrecto. Respuesta correcta: ' + data.answer + '</div>')
+  }
+  if(questionNum === questions.length) {
+    $('#fourth').hide();
+    //DISPLAY WAITING FOR OPONENT TO FINISH
+  }
+});
+
+socket.on('HELLO_ACK', (data) => {
+  $('#first').hide();
+  $('#second').show();
+});
+
+socket.on('GAME_START', (data) => {
+  questions = data.message;
+  $('#second').hide();
+  $('#score').show();
+  $('#score').html('<p><strong>Score: ' + score + ' / ' + questions.length + '</strong></p>');
+  next();
+});
+
+socket.on('GAME_OVER', (data) => {
+  $('#third').hide();
+  $('#result').hide();
+  $('#winner').show();
+  if(!data.winner) {
+    $('#winner').html('<h2>Empate!</h2>');
+  } else $('#winner').html('<h2>El ganador es: ' + data.winner + '</h2>');
+});
+
+$(document).ready(function(){
+  $('#second').hide();
+  $('#third').hide();
+  $('#fourth').hide();
+  $('#result').hide();
+  $('#score').hide();
+  $('#winner').hide();
+});
---
+++ server.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var _ = require('lodash');
+
+const questionsData = require('./data/questions.json');
+const COUNT = process.env.Q;
+
+var questions = [];
+var rooms = [getNewRoom()];
+
+setQuestionsSubjects();
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('TCP connection established');
+
+  var room;
+
+  socket.on('HELLO', (data) => {
+    console.log("New connection: " + data);
+    room = setPlayerOnRoom(data, socket);
+    socket.join(room.room);
+    socket.emit('HELLO_ACK', { message: 'Esperando oponente'});
+
+    if(Object.keys(room.players).length < 2) return;
+    startGame(room);
+  });
+
+  socket.on('ANSWER', (data) => {
+    var answeredQuestion = questions[data.id];
+    if(data.answer == answeredQuestion.answer) {
+      room.players[socket.id].score += 1;
+      console.log(room.players[socket.id].name + ' scores 1!');
+      socket.emit('ANSWER_ACK', {message: 'Correct!', correct: true});
+    } else socket.emit('ANSWER_ACK', {message: 'Incorrect', correct: false, answer: answeredQuestion.answer});
+
+    room.players[socket.id].submitted += 1;
+    checkGameOver(room);
+  });
+});
+
+function checkGameOver(room) {
+  var submittedPlayers = Object.keys(room.players);
+  if(room.players[submittedPlayers[0]].submitted == COUNT && room.players[submittedPlayers[1]].submitted == COUNT) {
+    var diff = room.players[submittedPlayers[0]].score - room.players[submittedPlayers[1]].score;
+    if(diff > 1) {
+      console.log(room.players[submittedPlayers[0]].name + ' WINS!');
+      io.sockets.in(room.room).emit('GAME_OVER', { winner: room.players[submittedPlayers[0]].name});
+    } else if(diff < 0) {
+      console.log(room.players[submittedPlayers[1]].name + ' WINS!');
+      io.sockets.in(room.room).emit('GAME_OVER', { winner: room.players[submittedPlayers[1]].name});
+    } else {
+      console.log('EVEN.');
+      io.sockets.in(room.room).emit('GAME_OVER', { winner: ''});
+    }
+  }
+}
+
+function getNewPlayer(player, socket) {
+  return { name: player, socket: socket, score: 0, submitted: 0 };
+}
+
+function getNewRoom() {
+  return { players: {}, questions: {}, room: new Date().getTime()};
+}
+
+function startGame(room) {
+  var sockets = Object.keys(room.players);
+  setQuestionsForRoomPlayer(room, sockets[0]);
+  setQuestionsForRoomPlayer(room, sockets[1]);
+
+  room.players[sockets[0]].socket.emit('GAME_START', {message: room.questions[sockets[0]]});
+  room.players[sockets[1]].socket.emit('GAME_START', {message: room.questions[sockets[1]]});
+}
+
+function setQuestionsForRoomPlayer(room, player) {
+  var indexUsed = [];
+  var index;
+  room.questions[player] = [];
+  for (var i = 0; i < COUNT; i++) {
+    index = Math.floor(Math.random()*(questions.length));
+    while(indexUsed.indexOf(index) > -1) {
+      index = Math.floor(Math.random()*(questions.length));
+    }
+    var selected = _.clone(questions[index]);
+    selected.id = index;
+    delete selected["answer"];
+    room.questions[player].push(selected);
+  }
+}
+
+function setPlayerOnRoom(player, socket) {
+  const last = rooms.length - 1;
+  if(Object.keys(rooms[last].players).length < 2) {
+    rooms[last].players[socket.id] = getNewPlayer(player, socket);
+    return rooms[last];
+  }
+
+  var newRoom = getNewRoom();
+  newRoom.players[socket.id] = getNewPlayer(player, socket);
+  rooms.push(newRoom);
+  return newRoom;
+}
+
+function setQuestionsSubjects() {
+  Object.keys(questionsData).map((k) => {
+    questionsData[k].forEach((q) => {
+      q.subject = k;
+    })
+  });
+
+  questions = _.flatten(_.values(questionsData));
+}
+
+server.listen(8080, () => {
+  console.log("Servidor corriendo en http://localhost:8080");
+});
